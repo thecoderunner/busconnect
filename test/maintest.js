e15e2b1ba0b178eb16e075ee0dd3206bb54e1741
thecoderunner <suitup9000@gmail.com>
2017-06-13 14:39:48 +1000

    Added test runner page

--- test/maintest.js
+++ test/maintest.js
@@ -1,7 +1,8 @@
-var expect = require("chai").expect;
 
-describe('Sanity check', function(){
-    it('Check mocha is setup properly',function(){
-        expect(true).to.be.ok;
+var expect = chai.expect;
+
+describe('Check functions are working', function(){
+    it('Check signature is calculated correctly',function(){        
+        expect(generatehash("/v3/departures/route_type/2/stop/10885/route/952?direction_id=29&devid=3000187", key).toString()).to.equal("5f01f1820eecdbd0743b3c153d3ffac43855e44a");
     });
 });
